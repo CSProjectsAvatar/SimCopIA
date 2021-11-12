c0f2327bfbe9233f6aada8f503c1740c4480ce15
Omar <omarohernan43@gmail.com>
2021-11-12 16:41:42 -0500

    Params Seted

--- Gos/Core/MetaHeuristics.cs
+++ Gos/Core/MetaHeuristics.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Core
 {
     public class MetaHeuristics
     {
+        public MetaHeuristics(){
+            MetaH(new List<Individual>(){},
+             (Individual i) => {return i.numero;}, 
+             (Individual i) => {return 0 < i.numero;});
+        }
+
+        private void MetaH(List<Individual> individuals, Func<Individual, int> mini, Func<Individual, bool> validate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Individual{
+        public int numero;
     }
 }
