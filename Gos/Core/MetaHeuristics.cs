2df06e435199f224c104345cbf617e415800e6fc
ClauP99 <claudinapuentess@gmail.com>
2021-11-14 10:55:34 -0800

    agregue getChildren

--- Gos/Core/MetaHeuristics.cs
+++ Gos/Core/MetaHeuristics.cs
@@ -19,7 +19,7 @@ namespace Core
         private List<int> getParents(List<Individual> individuals, Func<Individual, int> mini)
         {
             int count = individuals.Count;
-            int len = count % 2 == 1 ? count / 2 + 1 : count / 2;
+            int len = (int)Math.Ceiling(count * 0.6);
             List<int> pre_mini = new List<int> { };
 
             foreach (var item in individuals)
@@ -37,9 +37,30 @@ namespace Core
 
             return best_mini;
         }
-        
 
-    public class Individual{
+        private List<Individual> getChildren(List<int> parents)
+        {
+            List<Individual> children = new List<Individual> { };
+            int childrenCount = 0;
+            Random random1 = new Random();
+            Random random2 = new Random();
+
+            while (childrenCount != parents.Count)
+            {
+                var parent1 = parents[random1.Next(0, parents.Count - 1)];
+                var parent2 = parents[random2.Next(0, parents.Count - 1)];
+                children.Add(generateChildren(parent1, parent2));
+                childrenCount++;
+            }
+            return children;
+        }
+
+        private Individual generateChildren(int parent1, int parent2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Individual{
         public int numero;
     }
 }
