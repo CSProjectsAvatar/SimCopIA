75deb27564f10a5797e5368c26a7777784a757bd
Andy <aledesmacc@gmail.com>
2021-11-14 12:44:38 -0500

    1 Leader & Many Followers
    - Simulator done and tested.
    - Tested added to parallel simulator.
    - Extensions method to comparing double for equality.

--- Gos/Tests/OneLeaderFollowsSimtorTests.cs
+++ Gos/Tests/OneLeaderFollowsSimtorTests.cs
@@ -1,36 +1,43 @@
-using Core;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Core;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tests {
     [TestClass]
-    public class ParallelServerSimulatorTests : BaseTest {
-        private ILogger<ParallelServerSimulator> _log;
+    public class OneLeaderFollowsSimtorTests : BaseTest {
+        private ILogger<OneLeaderFollowersSimulator> _log;
 
         [TestInitialize]
         public void Init() {
-            _log = LoggerFact.CreateLogger<ParallelServerSimulator>();
+            //_log = Container.GetRequiredService<ILogger<OneLeaderFollowersSimulator>>();
+            _log = LoggerFact.CreateLogger<OneLeaderFollowersSimulator>();
         }
 
         [TestMethod]
         public void ArrivalsBeforeDepartures() {
-            var simtor = new ParallelServerSimulator(5,_log);
-            simtor.Run(100);
+            var simtor = new OneLeaderFollowersSimulator(5, _log);
+            simtor.Run(1000);
+
+            var deps = simtor.GetDepartures();
 
             Assert.IsTrue(simtor.Arrivals.Keys
-                .All(k => simtor.Arrivals[k] < simtor.Deapertures[k]));
+                .All(k => simtor.Arrivals[k] < deps[k]));
         }
 
         [DataTestMethod]
         [DataRow(20u, 5u)]
+        [DataRow(13u, 10u)]
+        [DataRow(5u, 103u)]
         public void FinishedRun(uint times, uint followers) {
             for (int i = 0; i < times; i++) {
-                var simtor = new ParallelServerSimulator(followers, _log);
+                var simtor = new OneLeaderFollowersSimulator(followers, _log);
 
                 Logger.LogMessage($"\nCorrida {i + 1}:\n");
 
