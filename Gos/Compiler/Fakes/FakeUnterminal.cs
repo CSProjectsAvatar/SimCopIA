640c911662bb91f3f9dd1d383de73a66431f8aa6
Omar <omarohernan43@gmail.com>
2021-12-17 12:24:37 -0500

    feat: iniciando unterminals

--- Gos/Compiler/Fakes/FakeUnterminal.cs
+++ Gos/Compiler/Fakes/FakeUnterminal.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using DataClassHierarchy;
 
 namespace Compiler {
     /// <summary>
---
+++ Gos/Compiler/Grammar Unterminals/ArgListUnt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataClassHierarchy;
+
+namespace Compiler {
+    internal class ArgListUnt : Unterminal
+    {
+        public IEnumerable<string> Args { get; set; }
+
+        //<arg-list> := ID | ID "," <arg-list>
+        protected override AstNode SetAst(IReadOnlyList<GramSymbol> derivation)
+        {
+            Args = new List<string>();
+            Args.Add((derivation[0] as Token).Lexem);
+            if(derivation.Count > 1){
+                var argList = (derivation[2] as ArgListUnt).Args;
+                Args.Append(argList);
+            }
+            return null;
+        }
+    }
+}
