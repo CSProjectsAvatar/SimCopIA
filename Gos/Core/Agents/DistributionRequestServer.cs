422995d0be0c4fac7c988df45f2fbf0aa2db38c8
ClauP99 <claudinapuentess@gmail.com>
2021-12-26 00:23:34 -0500

    mas funciones

--- Gos/Core/Agents/DistributionRequestServer.cs
+++ Gos/Core/Agents/DistributionRequestServer.cs
@@ -9,22 +9,60 @@ namespace Agents
     class DistributionRequestServer : Agent
     {
         public Dictionary<string, List<Agent>> requestNeeds;
+        public Dictionary<Request, List<Request>> originalRequest;
+        public Dictionary<int, string> responseRequest;
 
         public DistributionRequestServer(Environment env, string ID) : base(env, ID)
         {
+            originalRequest = new Dictionary<Request, List<Request>> { };
+            responseRequest = new Dictionary<int, string> { };
+           
         }
 
+       
         private void GettingRequestServer( Request r)
         {
+            originalRequest.Add(r, new List<Request> { });
+            responseRequest.Add(r.ID, "");
             if (requestNeeds.ContainsKey(r.URL))
             {
                 List<Agent> servers = requestNeeds[r.URL];
                 foreach (var item in servers)
                 {
-                    item.HandleRequest(new Request(this.ID, item.ID, environment));
+                    Request new_request = new Request(this.ID, item.ID, environment);
+                    environment.AddRequest(new_request,10);//aqui que time le pongo
+                    originalRequest[r].Add(new_request);
                 }
             }
 
+        }
+
+        private void GiveResponse(Response r)
+        {
+            bool mark = false;
+            foreach (var item in originalRequest.Keys)
+            {
+                foreach (var req in originalRequest[item])
+                {
+                    if (req.ID == r.requestID)
+                    {
+                        mark = true;
+                        responseRequest[item.ID] += r.body;
+                        originalRequest[item].Remove(req);
+                        if (originalRequest[item].Count == 0)
+                        {
+                            var response = new Response(item.ID, this, item.sender, environment, responseRequest[item.ID]);
+                            environment.SubscribeResponse(response);
+                            responseRequest.Remove(item.ID);
+                            originalRequest.Remove(item);
+                            //para que es AddSolutionResponse
+                        }
+                        break;
+                    }
+                }
+                if (mark)
+                    break;
+            }
         }
     }
   
