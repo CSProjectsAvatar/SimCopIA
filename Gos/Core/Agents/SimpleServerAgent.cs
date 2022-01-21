19e5d483e175100a05536ae5c8edfd44e3240b48
Mauricio <mauxms1996@gmail.com>
2022-01-20 21:19:35 -0500

    feat: Funciones Separadas en Workers
    + Se creo PredifinedFunc.cs que contiene funciones por buildin.
    + Se sacaron todas las funciones que usaban los workers de su clase.

--- Gos/Core/Agents/SimpleServerAgent.cs
+++ Gos/Core/Agents/SimpleServerAgent.cs
@@ -8,56 +8,16 @@ namespace Agents
         public Worker (Environment env, string ID, int totalRequests = 1): base(env, ID){
             this.TotalRequests = totalRequests;
 
-            this.functionsToHandleRequests.Add(this.GettingRequest);
-            this.functionsToHandleRequests.Add(this.ProcessRequest);
+            this.functionsToHandleRequests.Add(PredefinedFunctions.Workers.WorkerGettingRequest);
+            this.functionsToHandleRequests.Add(PredefinedFunctions.Workers.WorkerProcessRequest);
             
-            this.functionsToHandleStatus.Add(this.SendResponse);
-            this.functionsToHandleStatus.Add(this.SetAvailableAfterSendResponse);
+            this.functionsToHandleStatus.Add(PredefinedFunctions.Workers.WorkerSendResponse);
+            this.functionsToHandleStatus.Add(PredefinedFunctions.Workers.WorkerSetAvailableAfterSendResponse);
         }
 
        // public Worker (Environment env, string ID, List<Action<...> functions, int totalRequests = 1): base(env, ID, functions){
 
-        
-
-        private void GettingRequest(IRequestable status, Request r){
-            if(!status.IsAvailable){ 
-                
-                var res = new Response(r.ID,status.agent,r.sender,status.environment,"Servidor no disponible");
-                res.SetTime(status.environment.currentTime);
-                status.AddEvent(status.environment.currentTime, res );
-                status.environment.PrintAgent(status.agent,"Llega requst pero no esta disponible.");
-                return;
-            }
-            
-            environment.PrintAgent(status.agent,$"LLega request desde {r.sender}.");
-
-                        
-            Observer o = new Observer(status.agent,status.environment, r);
-
-            int time = environment.currentTime + TimeOffset();
-            status.AddEvent(time,o);
-        }
-        private void ProcessRequest(IRequestable status, Request r)
-        {
-            if (status.IsAvailable)
-            {
-                status.AddRequestToProcessed(r);
-                if (status.GetListRequest().Count >= TotalRequests)
-                    status.SetAvailibility(false);
-            }
-        }
-
-        private void SendResponse(IObservable status,Observer o){
-            Request originalRequest = (o.Object as Request);
-            Response response = new(originalRequest.ID,status.agent,originalRequest.sender,status.environment,$"Cosas de servidor simple {status.agent.ID}.");
-            response.SetTime(environment.currentTime);
-            status.AddEvent(environment.currentTime,response);
-        }
-        private void SetAvailableAfterSendResponse(IObservable status, Observer o){
-            Request originalRequest = (o.Object as Request);
-            status.DeleteRequest(originalRequest);            
-            status.SetAvailibility(true);
-        }
-        private int TimeOffset() => new Random().Next(5,10);
+       
+        public static int TimeOffset() => new Random().Next(5,10);
     } 
 }
